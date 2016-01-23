065b067e26d148413e26dbd94f03fbd4290041c8
Bc. Matěj Kříž <krizmate@fit.cvut.cz>
2016-01-23 16:18:44 +0100

    edit mode turning on and off on doubleclick
    Former-commit-id: 4c4d52da2f5fc502840c3aad63cd7e71cd29b525

--- app/js/services/ImageEditService.js
+++ app/js/services/ImageEditService.js
@@ -2,15 +2,39 @@
   'use strict';
   define(['Hammer'], function(Hammer) {
     var factory = function(Canvas) {
+      var ctx;
+      var img;
+      var manager;
+      var width, height;
 
-      //TODO: bind events only after doubleclick
 
-      function init(ctx, img, width, height) {
+      function turnOff() {
+        manager.off('rotatemove', rotateMove);
+        manager.off('rotateend', rotateEnd);
+        manager.off('panmove', panMove);
+        manager.off('pinchmove', pinchMove);
+        manager.off('pinchend', pinchEnd);
+      }
+
+      function turnOn() {
+        // subscribe to events
+        manager.on('rotatemove', rotateMove);
+        manager.on('rotateend', rotateEnd);
+        manager.on('panmove', panMove);
+        manager.on('pinchmove', pinchMove);
+        manager.on('pinchend', pinchEnd);
+      }
+
+      function init(context, image, w, h) {
+        ctx = context;
+        img = image;
+        width = w;
+        height = h;
         // get a reference to an element
         var stage = document.getElementById('previewCanvas');
 
         // create a manager for that element
-        var manager = new Hammer.Manager(stage);
+        manager = new Hammer.Manager(stage);
 
         // create recognizers
         var Pan = new Hammer.Pan({
@@ -30,59 +54,62 @@
         manager.add(Pan);
         manager.add(Rotate);
         manager.add(Pinch);
+      }
 
-        // subscribe to events
-        var liveScale = 1;
-        var prevRotation = 0;
-        var firstEvent = true;
-        manager.on('rotatemove', function(e) {
-            var rotation = Math.round(liveScale * e.rotation);
-            if (firstEvent) {
-              prevRotation = rotation;
-              firstEvent = false;
-            }
-            Canvas.canvasClear(ctx);
-            ctx.translate(width / 2, width / 2);
-            ctx.rotate((rotation - prevRotation) * Math.PI / 180);
-            ctx.translate(-width / 2, -width / 2);
-            ctx.drawImage(img, 0, 0, width, height);
-            prevRotation = rotation;
-        });
-        manager.on('rotateend', function(e) {
-          firstEvent = true;
-        });
-
-
-        manager.on('panmove', function(e) {
-          e.preventDefault();
-          if (img.width > width || img.height > height) {
-            Canvas.canvasClear(ctx);
-            ctx.translate(e.deltaX / 40, e.deltaY / 40);
-            ctx.drawImage(img, 0, 0, width, height);
-          }
-        });
-
-        // subscribe to events
-        var currentScale = 1;
-
-        function getRelativeScale(scale) {
-          return currentScale + (scale - 1) * 0.01;
-        }
-        manager.on('pinchmove', function(e) {
-          var scale = getRelativeScale(e.scale);
+      function panMove(e) {
+        e.preventDefault();
+        if (img.width > width || img.height > height) {
           Canvas.canvasClear(ctx);
-          ctx.scale(scale, scale);
+          ctx.translate(e.deltaX / 40, e.deltaY / 40);
           ctx.drawImage(img, 0, 0, width, height);
-        });
-        manager.on('pinchend', function(e) {
-          // cache the scale
-          currentScale = getRelativeScale(e.scale);
-          liveScale = currentScale;
-        });
+        }
+      }
+
+      function pinchEnd(e) {
+        // cache the scale
+        currentScale = getRelativeScale(e.scale);
+        liveScale = currentScale;
+      }
+
+      var currentScale = 1;
+
+      function getRelativeScale(scale) {
+        return currentScale + (scale - 1) * 0.01;
+      }
+
+      function pinchMove(e) {
+        var scale = getRelativeScale(e.scale);
+        Canvas.canvasClear(ctx);
+        ctx.scale(scale, scale);
+        ctx.drawImage(img, 0, 0, width, height);
+      }
+
+      function rotateEnd(e) {
+        firstEvent = true;
+      }
+
+      var liveScale = 1;
+      var prevRotation = 0;
+      var firstEvent = true;
+
+      function rotateMove(e) {
+        var rotation = Math.round(liveScale * e.rotation);
+        if (firstEvent) {
+          prevRotation = rotation;
+          firstEvent = false;
+        }
+        Canvas.canvasClear(ctx);
+        ctx.translate(width / 2, width / 2);
+        ctx.rotate((rotation - prevRotation) * Math.PI / 180);
+        ctx.translate(-width / 2, -width / 2);
+        ctx.drawImage(img, 0, 0, width, height);
+        prevRotation = rotation;
       }
 
       return {
-        init: init
+        init: init,
+        turnOn: turnOn,
+        turnOff: turnOff
       };
     };
 
